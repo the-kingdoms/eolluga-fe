342e3d8a56d7798080fbe397e3f735046199e033
yerim08 <sbnlover1008@naver.com>
2024-07-25 00:27:56 +0900

    FIX: Origin 컴포넌트 스타일 수정

--- src/widgets/origin-info/ui/Origin.tsx
+++ src/widgets/origin-info/ui/Origin.tsx
@@ -7,7 +7,7 @@ export default function Origin({ contents }: OriginProps) {
   return (
     <>
       <BackButtonWithTitle title="원산지 정보" />
-      <div className="font-Pretendard mx-auto mt-[18px] w-[328px] text-justify text-[12px] font-normal leading-[18px] tracking-[-0.25px]">
+      <div className="font-Pretendard align-center mx-auto mt-[18px] w-full px-[16px] text-justify text-[12px] font-normal leading-[18px] tracking-[-0.25px]">
         {contents}
       </div>
     </>
