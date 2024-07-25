c17e1c3a7871c7e8641b184f9d8fea9b0d476de5
KimYouGyoung <tptkds12@gmail.com>
2024-07-25 17:20:41 +0900

    💄 layout: 빈 장바구니 텍스트 수정

--- src/widgets/cart/ui/EmptyCart.tsx
+++ src/widgets/cart/ui/EmptyCart.tsx
@@ -4,12 +4,7 @@ export default function EmptyCart() {
   return (
     <div className="absolute top-0 flex h-svh w-full flex-col items-center justify-center space-y-[24px]">
       <Image src="/empty-cart.png" width={284} height={238} alt="빈 카트" />
-      <Image
-        src="/empty-cart-text.png"
-        width={299}
-        height={36}
-        alt="메뉴를 담으러 가볼까요?"
-      />
+      <p className="text-[36px] font-bold">메뉴를 담으러 가볼까요?</p>
     </div>
   );
 }
