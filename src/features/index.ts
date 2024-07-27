08764a3306530e0a75eb328ad779660f5e30a14b
yerim08 <68000705+yerim08@users.noreply.github.com>
2024-07-27 16:51:06 +0900

    FIX: lint 에러 수정

--- src/features/index.ts
+++ src/features/index.ts
@@ -1,4 +1,3 @@
 export { default as CallStaff } from "./call-staff/ui/CallStaff";
 export { default as OrderMenu } from "./order-menu/ui/OrderMenu";
 export { default as DeleteAllCartItems } from "./delete-all-cart-items/ui/DeleteAllCartItems";
-
