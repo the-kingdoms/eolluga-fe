9dafe8a61862d59dd77c8af944dea11e7dc4d4eb
KimYouGyoung <tptkds12@gmail.com>
2024-07-29 17:21:31 +0900

    🔥 remove: 중복 코드 제거

--- src/widgets/order-list/ui/OrderItems.tsx
+++ src/widgets/order-list/ui/OrderItems.tsx
@@ -28,7 +28,6 @@ export default function OrderItems({ order }: { order: OrderItemT }) {
 
       <div
         className={`origin-top transform transition-all duration-300 ease-in-out ${showMore[order.orderId] ? "max-h-[1000px] scale-y-100 opacity-100" : "max-h-0 scale-y-0 opacity-0"}`}
-        style={{ transformOrigin: "top" }}
       >
         {order.items.map(item => (
           <li className="relative border-t py-[20px]" key={item.id}>
