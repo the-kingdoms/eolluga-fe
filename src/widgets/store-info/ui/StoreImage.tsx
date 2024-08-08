f1f86b567fc7faa46462c6f90b26c2773f67f432
KimYouGyoung <tptkds12@gmail.com>
2024-08-08 21:06:09 +0900

    CHORE: 매장 디폴트 이미지를 public 폴더에 추가
    - store 객체가 가지고 있는 이미지 url이 null인 경우 기본 이미지를 보여주기 위함

--- src/widgets/store-info/ui/StoreImage.tsx
+++ src/widgets/store-info/ui/StoreImage.tsx
@@ -4,10 +4,7 @@ export default function StoreImage({ imageSrc }: { imageSrc: string }) {
   return (
     <div className="relative h-[240px] w-full text-center">
       <Image
-        src={
-          imageSrc ||
-          "https://eobkvusotnvurtdlxvgr.supabase.co/storage/v1/object/sign/eolluga/image.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJlb2xsdWdhL2ltYWdlLmpwZyIsImlhdCI6MTcyMjg1MDQ0MCwiZXhwIjoxNzU0Mzg2NDQwfQ.aiAQ4TZgsm1LvufWu5-voDWsytkq1LoaWMtv3XxmXFo&t=2024-08-05T09%3A33%3A59.819Z"
-        }
+        src={imageSrc || "/image/default-store.png"}
         alt="매장 이미지"
         sizes="100vw"
         style={{ objectFit: "cover" }}
