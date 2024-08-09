e39bff4e078c6272eba17240658a97c432121968
finn <tptkds12@gmail.com>
2024-08-09 19:01:35 +0900

    REFACTOR: 얼루가 MenuOptions API 연결
    - src/app/[storeId]/[tableId]/menu/detail/[menuId]/page.tsx에서 서버 사이드 패칭 후 MenuInfo props으로 넘기는 부분까지 작성

--- src/app/[storeId]/[tableId]/menu/detail/[menuId]/page.tsx
+++ src/app/[storeId]/[tableId]/menu/detail/[menuId]/page.tsx
@@ -1,11 +1,18 @@
+import { getMenuOptions } from "@/entities";
 import TopBar from "@/shared/ui/TopBar";
 import { MenuInfo } from "@/widgets";
 
-export default function Page() {
+export default async function Page({
+  params,
+}: {
+  params: { menuId: string; storeId: string };
+}) {
+  const menuOptions = await getMenuOptions(params.storeId, params.menuId);
+  console.log(menuOptions);
   return (
     <>
       <TopBar storeId={1} tableId={1} showBackButton showCartLink />
-      <MenuInfo />
+      <MenuInfo data={menuOptions} />
     </>
   );
 }
---
+++ src/entities/menu-options/api/getMenuOptions.ts
@@ -0,0 +1,24 @@
+import { BASE_URL } from "@/shared";
+
+import { MenuOptionT } from "./menu-options";
+
+type ResponseData = MenuOptionT[] | [];
+const getMenuOptions = async (
+  storeId: string,
+  menuId: string,
+): Promise<ResponseData> => {
+  const res = await fetch(
+    `${BASE_URL}/api/menu-options?storeId=${storeId}&menuId=${menuId}`,
+  );
+  if (res.ok) {
+    const data: MenuOptionT[] = await res.json();
+    return data;
+  }
+  const errorData = await res.json();
+
+  console.error(errorData || "알 수 없는 오류 발생");
+  return [];
+  // throw new Error(errorData.message || "알 수 없는 오류 발생");
+};
+
+export default getMenuOptions;
