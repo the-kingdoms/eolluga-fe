34373e440dc9cc2a562d1ec90729490ae9e95a2c
developerryou <developerryou@gmail.com>
2024-10-05 16:05:19 +0900

    FIX: 카트 버튼 클릭시 페이지 이동

--- src/widgets/view-cart/ui/ViewCartButton.tsx
+++ src/widgets/view-cart/ui/ViewCartButton.tsx
@@ -2,9 +2,12 @@
 
 import { useEffect, useState } from "react";
 
+import { useRouter } from "next/navigation";
+
 import { calculateCartTotalPrice } from "@/shared";
 
 export default function ViewCartButton() {
+  const { push } = useRouter();
   const [total, setTotal] = useState(0);
 
   useEffect(() => {
@@ -19,6 +22,9 @@ export default function ViewCartButton() {
         type="button"
         className="flex h-[64px] w-full items-center justify-center space-x-[16px] rounded-lg bg-[#131313] text-[20px] text-white"
         aria-label={`합계 ${total.toLocaleString()}원 장바구니 보기`}
+        onClick={() => {
+          push("./cart");
+        }}
       >
         <span className="font-bold">{total.toLocaleString()}원</span>{" "}
         <span className="text-[#8D8D8D]">|</span>
