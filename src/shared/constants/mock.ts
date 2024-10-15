e738d659339a7eb5b89a1e975cb4b3ca450bde75
developerryou <developerryou@gmail.com>
2024-10-15 19:44:12 +0900

    FIX: env 통해 feturl 수정

--- src/shared/constants/mock.ts
+++ src/shared/constants/mock.ts
@@ -1,5 +1,7 @@
 export const MOCK_SERVER_URL =
   "https://7cb3f3eb-68d0-41b7-b17c-36e2246fc3ab.mock.pstmn.io";
 
-export const BASE_URL = "http://localhost:3000";
-export const SERVICE_URL = "https://api.eolluga.com/api/v1";
+export const BASE_URL =
+  process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+export const SERVICE_URL =
+  process.env.NEXT_PUBLIC_SERVICE_URL || "https://api.eolluga.com/api/v1";
